2abefae072b7f589372812ae7d6284f6b296f441
Tapishagrawal <tapishg4@gmail.com>
2024-04-18 21:44:16 +0530

    chage loading indicator color

--- src/components/Loadning.jsx
+++ src/components/Loadning.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Loadning() {
     return (
         <div className='fixed left-[50%] top-20 -translate-x-[50%]'>
-            <div className="w-16 h-16 border-8 border-dashed rounded-full animate-spin border-blue-600"></div> 
+            <div className="w-16 h-16 border-8 border-dashed rounded-full animate-spin border-orange-700"></div> 
         </div>
 
     )
