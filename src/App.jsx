3edb890f121e767540f63a392990a6e2b03c304a
Tapishagrawal <tapishg4@gmail.com>
2024-04-19 15:14:05 +0530

    serach

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { useEffect } from 'react'
 import './App.css'
 import AllRoutes from './components/AllRoutes'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
+import { useDispatch } from 'react-redux'
+import { fetchAllPokeonData } from './redux/action'
 
 function App() {
+  const dispatch = useDispatch();
 
+  useEffect(()=>{
+    dispatch(fetchAllPokeonData())
+  },[])
   return (
     <>
       <Navbar/>
